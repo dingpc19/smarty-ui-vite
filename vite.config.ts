5f961b593a1ef9aa49e1845e941e5d2b73709527
dpc19 <714098681@qq.com>
2023-05-14 17:38:19 +0800

    feat: github-workflow

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,9 @@ export default defineConfig({
 
   build: {
     rollupOptions,
-    minify: false,
+    minify: 'terser', // boolean | 'terser' | 'esbuild'
+    sourcemap: true, // 输出单独 source文件
+    reportCompressedSize: true, // 生成压缩大小报告
     cssCodeSplit: true,
     lib: {
       entry: './src/entry.ts',
