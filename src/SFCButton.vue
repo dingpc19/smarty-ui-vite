6a2150096c8eeb6caf5383bcc1d763bb2e965fa5
dpc19 <714098681@qq.com>
2023-05-14 09:06:09 +0800

    单文件组件SFC

---
+++ src/SFCButton.vue
@@ -0,0 +1,13 @@
+<!--
+ * new page
+ * @author: dingpengcheng
+ * @since: 2023-05-14
+ * SFCButton.vue
+-->
+<template>
+  <div>SFCButton.vue</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
